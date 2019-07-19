22f7ffb21bafa5882a2edc2ef99b3450428184f2
Harrison <harrisonized@gmail.com>
2019-07-19 14:21:45 -0700

    bar plot

--- python-scripts/plot-week-data.py
+++ python-scripts/plot-week-data.py
@@ -14,16 +14,40 @@ pd.set_option("precision", 3)
 
 
 
-turnstile_norm = pd.read_csv(r"data/turnstile/concat-csv/turnstile-edt-cat.csv")
-turnstile_norm = turnstile_norm.rename(columns = {"Unnamed: 0" : "DateTime"}).set_index("DateTime") # Formatting
+turnstile_edt_df = pd.read_csv(r"data/turnstile/concat-csv/turnstile-edt-cat.csv")
+turnstile_edt_df = turnstile_edt_df.rename(columns = {"Unnamed: 0" : "DateTime"}).set_index("DateTime") # Formatting
 
 
 
-turnstile_dst = pd.read_csv(r"data/turnstile/concatenated-csv/turnstile-est-cat.csv")
-turnstile_dst = turnstile_dst.rename(columns = {"Unnamed: 0" : "DateTime"}).set_index("DateTime") # formatting
+turnstile_est_df = pd.read_csv(r"data/turnstile/concat-csv/turnstile-est-cat.csv")
+turnstile_est_df = turnstile_est_df.rename(columns = {"Unnamed: 0" : "DateTime"}).set_index("DateTime") # formatting
 
 
 
-turnstile_norm.iloc[:42, 0:10].plot(ylim = (0, 100000), xticks=None)
-plt.xticks(rotation=90)
+turnstile_edt_df.iloc[:42, 0:5].plot(ylim = (0, 100000), figsize=(12, 8))
+plt.xticks([0, 6, 12, 18, 24, 30, 36], ['Sat', 'Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri'])
+plt.xlabel('Day of the Week', fontsize=16)
+plt.ylabel('Number of People', fontsize=16)
+plt.title('One Week of Station Traffic for the Top 5 Stations', fontsize=24)
 plt.savefig("figures/week-data.png", bbox_inches="tight")
+
+
+
+# Generate list of top 15 stations
+top_station_dict = dict(turnstile_edt_df.iloc[:42, 0:15].sum()/1000000)
+top_station_dict = dict(zip(top_station_dict.values(), top_station_dict.keys()))
+top_station_count_list = sorted(top_station_dict)
+top_station_name_list = list(map(top_station_dict.get, top_station_count_list))
+
+
+
+# Plot
+y_pos = np.arange(15)
+fig, ax = plt.subplots(figsize=(12, 8))
+ax.barh(y_pos, top_station_count_list, align='center', capsize=3)
+ax.set_xlabel('Number of Riders Per Week (Millions)', fontsize=12)
+ax.set_yticks(y_pos)
+ax.set_yticklabels(top_station_name_list, fontsize=12)
+ax.set_title('Top 15 Stations for Ridership', fontsize=16)
+ax.xaxis.grid(True)
+plt.savefig("figures/top-15-stations-bar.png", transparent=True, bbox_inches="tight")
