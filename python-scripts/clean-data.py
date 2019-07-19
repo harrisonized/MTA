d1aa770664354605f011eb2ca96ebaa69720258b
Harrison <harrisonized@gmail.com>
2019-07-19 12:01:14 -0700

    changing global variable declarations in functions to return statements

--- python-scripts/clean-data.py
+++ python-scripts/clean-data.py
@@ -3,8 +3,7 @@ import pandas as pd
 import datetime as dt
 from collections import Counter
 import re
-import bs4
-import requests
+import glob # For opening and closing files
 import warnings # Turn off warnings
 warnings.filterwarnings('ignore')
 
@@ -21,8 +20,6 @@ def format_turnstile_df(turnstile_csv_df):
     turnstile_csv_df is the dataframe of the imported turnstile.csv file
     Warning: Run once per file ONLY. The function will throw an error if run twice on the same file.
     """
-
-    global turnstile_df
     turnstile_df = turnstile_csv_df
     
     # Reformatting
@@ -33,6 +30,8 @@ def format_turnstile_df(turnstile_csv_df):
     turnstile_df.DATETIMERAW = turnstile_df.DATETIMERAW.apply(lambda x : dt.datetime.strptime(x, "%m/%d/%Y%H:%M:%S")) # Convert DATETIMERAW into datetime object
     turnstile_df.TIME = turnstile_df.TIME.apply(lambda x : dt.datetime.strptime(x, "%H:%M:%S")) # Convert TIME into datetime object
     turnstile_df = turnstile_df.drop(columns = ['DATE']) # Drop DATE column
+    
+    return turnstile_df
 
 def valuecount(timestamp1, timestamp2):
     """
@@ -42,7 +41,6 @@ def valuecount(timestamp1, timestamp2):
     This function is not run as a standalone function. It is run within the grabweek() function.
 
     """
-    global value_count_df
     
     # Grabbing the data
     turnstile_df1 = turnstile_df.loc[turnstile_df['DATETIMERAW'].isin([timestamp1])].reset_index(drop=True) # Grab dataset for timestamp1
@@ -71,11 +69,13 @@ def valuecount(timestamp1, timestamp2):
     value_count_df.columns = ["ValueCount"] # Renaming column to ValueCount
     value_count_df = value_count_df.sort_values(by=["ValueCount"], ascending=False) # Sorting by count
 
-def generate_timestamp_list():
+    return value_count_df
+
+
+def generate_timestamp_list_edt():
     """
     This function defines the boundaries of the data pulled. It references turnstile_df, which is the output of format_turnstile_df().
     """ 
-    global timestamp_list
     datetimeraw_count = Counter(turnstile_df['DATETIMERAW'])
     
     i = 0
@@ -84,11 +84,13 @@ def generate_timestamp_list():
         timestamp_list.append(min(datetimeraw_count) + dt.timedelta(hours=4*i))
         i += 1
 
-def generate_timestamp_list_dst():
+    return timestamp_list
+
+
+def generate_timestamp_list_est():
     """
     This function defines the boundaries of the data pulled. It references turnstile_df, which is the output of format_turnstile_df().
     """ 
-    global timestamp_list
     datetimeraw_count = Counter(turnstile_df['DATETIMERAW'])
     
     i = 0
@@ -96,15 +98,16 @@ def generate_timestamp_list_dst():
     while min(datetimeraw_count) + dt.timedelta(hours=4*i) < max(datetimeraw_count):
         timestamp_list.append(min(datetimeraw_count) + dt.timedelta(hours=4*i+3))
         i += 1
+    
+    return timestamp_list
+
 
 def grabweek():
 	"""
 	This function grabs data for the whole week and enters it into a dataframe
 	This function references the timestamp_list and calls upon the valuecount() function.
 	"""
-    global value_count_df_1to2
-
-    valuecount(timestamp_list[0], timestamp_list[1])
+    value_count_df = valuecount(timestamp_list[0], timestamp_list[1])
     value_count_df_1to2 = value_count_df[["ValueCount"]]
     value_count_df_1to2.columns = ["{}".format(timestamp_list[0])]
 
@@ -113,22 +116,19 @@ def grabweek():
         value_count_df_1to2 = value_count_df_1to2.join(value_count_df[["ValueCount"]])
         value_count_df_1to2 = value_count_df_1to2.rename(columns={'ValueCount':"{}".format(str(timestamp_list[j]))})
 
+    return value_count_df_1to2
 
 
-# Grabs all the links in the mta webpage and collects the names for the last year into turnstile_link_list
-URL = "http://web.mta.info/developers/turnstile.html"
-soup = bs4.BeautifulSoup(urlopen(URL))
-all_link_list = []
-for link in soup.findAll('a'):
-    all_link_list.append(link.get('href'))
-turnstile_link_list = [all_link_list[36:89][i].replace("data/nyct/turnstile/", "") for i in range(len(link_list[36:89]))]
+
+# Show the files in the folder
+files_list = glob.glob("data/turnstile/downloads/*") # Grab a list of filenames
+filenames_list = list(map(lambda x: x.replace("data/turnstile/downloads/", ""), sorted(files_list, reverse = True)))
 
 
 
 # Import latlong_clean.csv, which was exported from a different notebook file.
-latlong_df = pd.read_csv(r"Coordinate Data/latlong_clean.csv")
+latlong_df = pd.read_csv(r"data/coordinate/latlong-clean.csv")
 latlong_df = latlong_df.drop(columns={"Unnamed: 0", "Numbering"}) # Dropping the "Unnamed: 0" and "Numbering" column
-latlong_df.head() # Preview the data
 
 
 
@@ -144,27 +144,25 @@ Notes:
 
 Warning: This may take a long time! On my computer, it outputs 2-3 files per minute and ran for 20 min+.
 """
-for i in turnstile_link_list[25:len(turnstile_link_list)]: # Note, if loop stops, restart at an appropriate lowerbound for range
+for i in filenames_list[25:len(filenames_list)]: # Note, if loop stops, restart at an appropriate lowerbound for range
 
-    turnstile_df = pd.read_csv(r"Turnstile Data/Downloads/{}".format(i)) # File imports
-    format_turnstile_df(turnstile_df) # Warning: Run once per file import ONLY
+    turnstile_csv_df = pd.read_csv(r"data/turnstile/downloads/{}".format(i)) # File imports
+    turnstile_df = format_turnstile_df(turnstile_csv_df) # Warning: Run once per file import ONLY
 
-    generate_timestamp_list() # Generating list of timestamps
-    grabweek() # Warning: May take a minute
+    timestamp_list = generate_timestamp_list_edt(turnstile_df) # Generating list of timestamps
+    value_count_df_1to2 = grabweek(timestamp_list) # Warning: May take a minute
     value_count_df_1to2 = value_count_df_1to2.reset_index().rename(columns={"index":"StationName"})
 
     # Merge coordinate data with value_count data
     value_count_df_1to2_with_coord = latlong_df.merge(value_count_df_1to2, on=["StationName"], how='left').reset_index(drop=True)
-    value_count_df_1to2_with_coord.head() # Preview the final data
 
-    value_count_df_1to2_with_coord.to_csv(r"Turnstile Data/Processed CSV/{}_proc.csv".format(i.replace(".txt","")))
-    
+    value_count_df_1to2_with_coord.to_csv(r"data/turnstile/processed-csv/{}_proc.csv".format(i.replace(".txt","")))    
 
 
 """
 The following is for AFTER running the above loop and downloading as many files as possible
 """
-turnstile_proc_list = [turnstile_link_list[i].replace(".txt", "")+"_proc.csv" for i in range(len(turnstile_link_list))]
+turnstile_proc_list = [filenames_list[i].replace(".txt", "")+"_proc.csv" for i in range(len(filenames_list))]
 turnstile_proc_list.pop(2) # Removing 'turnstile_190316_proc.csv' from list
 turnstile_proc_list.pop(19) # Removing 'turnstile_181110_proc.csv' from list
 
@@ -184,17 +182,16 @@ Warning: This may take a long time! On my computer, it outputs 2-3 files per min
 
 """
 
-for i in turnstile_link_list[3:21]: # Note, if loop stops, restart at an appropriate lowerbound for range
+for i in filenames_list[3:21]: # Note, if loop stops, restart at an appropriate lowerbound for range
+    
+    turnstile_csv_df = pd.read_csv(r"data/turnstile/downloads/{}".format(i)) # File imports
+    turnstile_df = format_turnstile_df(turnstile_csv_df) # Warning: Run once per file import ONLY
 
-    turnstile_df = pd.read_csv(r"Turnstile Data/Downloads/{}".format(i)) # File imports
-    format_turnstile_df(turnstile_df) # Warning: Run once per file import ONLY
-
-    generate_timestamp_list_dst() # Generating list of timestamps
-    grabweek() # Warning: May take a minute
+    timestamp_list = generate_timestamp_list_est(turnstile_df) # Generating list of timestamps
+    value_count_df_1to2 = grabweek(timestamp_list) # Warning: May take a minute 
     value_count_df_1to2 = value_count_df_1to2.reset_index().rename(columns={"index":"StationName"})
 
     # Merge coordinate data with value_count data
     value_count_df_1to2_with_coord = latlong_df.merge(value_count_df_1to2, on=["StationName"], how='left').reset_index(drop=True)
-    value_count_df_1to2_with_coord.head() # Preview the final data
 
-    value_count_df_1to2_with_coord.to_csv(r"Turnstile Data/Processed CSV/{}_proc.csv".format(i.replace(".txt","")))
+    value_count_df_1to2_with_coord.to_csv(r"data/turnstile/processed-csv/{}_proc.csv".format(i.replace(".txt","")))
